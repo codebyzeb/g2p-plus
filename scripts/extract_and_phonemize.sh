0767d803fa6597cd94a672e0273ffa88070f7c81
Zeb <zepsilon32@gmail.com>
2024-07-09 12:40:07 +0100

    Add scripts to process datasets in one go

--- scripts/extract_and_phonemize.sh
+++
@@ -1,4 +0,0 @@
-
-echo Extracting and phonemizing for language: $1
-python childes_processor.py extract childes/downloaded/$1 -o childes/processed/$1
-python childes_processor.py phonemize childes/processed/$1/adult.txt $1 -o childes/phonemized/$1 -s
