9c69448233c3bc03b6f439b4ac48070952a3fc37
Zeb <zepsilon32@gmail.com>
2024-10-14 14:40:54 +0100

    Remove BNC tools from src

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,5 @@
 """ Utility functions for the project. """
 
-import pandas as pd
-from .dicts import CONVERSION_TABLE
-
 def move_tone_marker_to_after_vowel(syll):
     """ Move the tone marker from the end of a cantonese syllable to directly after the vowel """
 
@@ -40,54 +37,3 @@ def move_tone_marker_to_after_vowel_line(line):
     for tone in tone_symbols:
         line = line.replace(' ' + tone, tone)
     return line
-
-def convert_bnc_to_ipa(phones):
-    """Converts a list of phones to IPA."""
-    ipa = []
-    i = 0
-    while i < len(phones):
-        # Deal with dipthongs
-        phone = phones[i].upper()
-        if i != len(phones) - 1 and (phone == 'IH1' or phone == 'IH2') and phones[i+1] == 'AH0':
-            ipa.append('ɪə')
-            i += 1
-        elif phone in CONVERSION_TABLE:
-            ipa.append(CONVERSION_TABLE[phone])
-        elif phone[-1] in ['0','1','2'] and phone[:-1] in CONVERSION_TABLE: # Stressed vowel
-            ipa.append(CONVERSION_TABLE[phone[:-1]])
-        else:
-            raise ValueError('Unknown phone: {}'.format(phone))
-        i += 1
-    return ipa
-
-# Tries to match the formatting of TextGrid words in BNC to the orthographic words in BNC
-def clean_textgrid_words(words):
-    word_line = ' '.join([word for word in words if not word in ['sp', '{OOV}', '{LG}', '{GAP_ANONYMIZATION}', '{CG}', '{XX}']])
-    word_line = (
-        word_line.replace(" 'S", "'S")
-        .replace(" 'VE", "'VE")
-        .replace("GON NA", "GONNA")
-        .replace("DUN N","DUN XXXXN")
-        .replace("DUN N NO","DUNNO")
-        .replace(" N IT","NIT")
-        .replace("GOT TA","GOTTA")
-        .replace("WAN NA","WANNA").strip()
-    )
-    return word_line
-
-def edit_distance(line1, line2):
-    # Initialize the matrix
-    matrix = [[0 for _ in range(len(line2) + 1)] for _ in range(len(line1) + 1)]
-    for i in range(len(line1) + 1):
-        matrix[i][0] = i
-    for j in range(len(line2) + 1):
-        matrix[0][j] = j
-    # Compute the matrix
-    for i in range(1, len(line1) + 1):
-        for j in range(1, len(line2) + 1):
-            if line1[i-1] == line2[j-1]:
-                matrix[i][j] = matrix[i-1][j-1]
-            else:
-                matrix[i][j] = min(matrix[i-1][j] + 1, matrix[i][j-1] + 1, matrix[i-1][j-1] + 1)
-    # Return the edit distance
-    return matrix[-1][-1]
